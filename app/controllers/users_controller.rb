da4712b09faaf0d99185d16224fdf2b3f8f9abf9
andresduarte <andresduarte010@gmail.com>
2016-12-01 14:18:31 -0500

    post login and post signup routes added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,15 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-
-  end
+    if params[:username] == "" || params[:email] == "" params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+      @user.save
+      session[:user_id] = @user.id
+      redirect '/films'
+    end
+   end
 
   get '/login' do
     if !logged_in?
@@ -19,6 +26,16 @@ class UsersController < ApplicationController
       redirect '/films'
     end
   end
-  
+
+  post '/login' do
+    @user = User.find_by(username: params["username"])
+    if @user && @user.authenticate(params["password"])
+      session[:user_id] = @user.id
+      redirect '/films'
+    else
+      redirect '/signup'
+    end
+  end
+
 
 end
