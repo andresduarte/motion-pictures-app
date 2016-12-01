312a80af86c00120104b56c609e7ae18aebec1c1
andresduarte <andresduarte010@gmail.com>
2016-12-01 14:51:12 -0500

    added index (welcoem + sign up/log in links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params[:username] == "" || params[:email] == "" params[:password] == ""
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect '/signup'
     else
       @user = User.new(username: params["username"], email: params["email"], password: params["password"])
