76857e9345dbf73d63f21e0e8e690c5f1a613f00
andresduarte <andresduarte010@gmail.com>
2016-12-01 14:05:15 -0500

    user_controller second route + log in view done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,18 @@ class UsersController < ApplicationController
       redirect '/films'
     end
   end
+
+  post '/signup' do
+
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/films'
+    end
+  end
   
+
 end
