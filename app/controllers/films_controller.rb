511020b6b81621c2f035c968bf031661c94c640c
andresduarte <andresduarte010@gmail.com>
2016-12-04 22:51:32 -0500

    added flash messages all around

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,4 +1,8 @@
+require 'rack-flash'
+
 class FilmsController < ApplicationController
+
+  use Rack::Flash
 
   get '/films' do
     if logged_in?
@@ -17,17 +21,6 @@ class FilmsController < ApplicationController
     end
   end
 
-  post '/films' do
-    if params["film"]["title"] == "" || params["film"]["genre"] == "" || params["film"]["runtime"] == "" || params["film"]["writer_ids"] == nil || params["film"]["director_ids"] == nil
-      redirect "/films/new"
-    else
-      @film = Film.create(params["film"])
-      @film.save
-      redirect "films/#{@film.slug}"
-    end
-  end
-
-
   get '/films/:slug' do
     if logged_in?
       @film = Film.find_by_slug(params[:slug])
@@ -37,12 +30,25 @@ class FilmsController < ApplicationController
     end
   end
 
+  post '/films' do
+    if params["film"]["title"] == "" || params["film"]["genre"] == "" || params["film"]["runtime"] == "" || params["film"]["writer_ids"] == nil || params["film"]["director_ids"] == nil
+      flash[:message] = "Please fill all fields to create film"
+      redirect "/films/new"
+    else
+      @film = Film.create(params["film"])
+      @film.save
+      flash[:message] = "Successfully created film"
+      redirect "films/#{@film.slug}"
+    end
+  end
+
   get '/films/:slug/edit' do
     if logged_in?
       @film = Film.find_by_slug(params[:slug])
       if @film.writers.any? { |writer| writer.user_id == current_user.id } || @film.directors.any? { |director| director.user_id == current_user.id}
         erb :'films/edit_film'
       else
+        flash[:message] = "You can't edit a film you didn't write or direct"
         redirect '/films'
       end
     else
@@ -52,11 +58,13 @@ class FilmsController < ApplicationController
 
   patch '/films/:slug' do
     if params["film"]["title"] == "" || params["film"]["genre"] == "" || params["film"]["runtime"] == "" || params["film"]["writer_ids"] == nil || params["film"]["director_ids"] == nil
+      flash[:message] = "Please fill all fields to edit film"
       redirect "films/#{params[:slug]}/edit"
     else
       @film = Film.find_by_slug(params[:slug])
       @film.update(params["film"])
       @film.save
+      flash[:message] = "Successfully updated song."
       redirect "/films/#{@film.slug}"
     end
   end
@@ -66,8 +74,10 @@ class FilmsController < ApplicationController
       @film = Film.find_by_slug(params[:slug])
       if @film.writers.any? { |writer| writer.user_id == current_user.id } || @film.directors.any? { |director| director.user_id == current_user.id}
         @film.destroy
+        flash[:message] = "Successfully deleted film"
         redirect '/films'
       else
+        flash[:message] = "You can't delete a film you didn't write or direct"
         redirect '/films'
       end
     else
